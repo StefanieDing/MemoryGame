9a973c29ed93f58a3d2486d89c99bd1cdf2126ef
Tomcariello <tomcariello@yahoo.com>
2016-08-22 20:33:14 -0400

    a few js functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var pickFirstNumber = true;
+var firstpick;
+var secondPick;
+var wins = 0;
+var losses = 0;
+var guesses = 5;
+
+
+$('.card').on('click', function(event) {
+	alert("clicked");
+	if (pickFirstNumber == true) {
+		fistPick = this.value;
+		pickFirstNumber = false;
+	} else if (pickFirstNumber == false) {
+		secondPick = this.value;
+		isMatched();
+	}
+});
+
+function isMatched() {
+	if (firstPick == secondPick) {
+		$('#msg-box').html("You Win!");
+		wins++;
+	} else {
+		guesses--;
+		if (guesses == 0) {
+			$('#msg-box').html("You lose!");
+			losses++;
+		}
+	}
+}
+
+
